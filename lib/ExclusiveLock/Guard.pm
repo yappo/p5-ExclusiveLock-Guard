9a9fcb386d4cc014e8b9e27b2dbf0c6ccaf61969
Kazuhiro Osawa <yappo@shibuya.pl>
2012-11-08 15:18:58 +0900

    initial commit

---
+++ lib/ExclusiveLock/Guard.pm
@@ -0,0 +1,32 @@
+package ExclusiveLock::Guard;
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+ExclusiveLock::Guard -
+
+=head1 SYNOPSIS
+
+    use ExclusiveLock::Guard;
+
+=head1 DESCRIPTION
+
+ExclusiveLock::Guard is
+
+=head1 AUTHOR
+
+Kazuhiro Osawa E<lt>yappo {at} shibuya {dot} plE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
